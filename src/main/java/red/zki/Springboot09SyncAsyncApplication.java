ad955dba5b991e6803458a8e8018c604286a094a
Administrator <727447170@qq.com>
2020-12-05 11:11:10 +0800

    first commit

---
+++ src/main/java/red/zki/Springboot09SyncAsyncApplication.java
@@ -0,0 +1,13 @@
+package red.zki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot09SyncAsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot09SyncAsyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/red/zki/Springboot09SyncAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package red.zki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot09SyncAsyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
