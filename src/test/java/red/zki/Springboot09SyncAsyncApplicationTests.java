e96cb264f22b8efd1353cd8a7b7b73c8512784b3
Administrator <727447170@qq.com>
2020-12-05 14:31:13 +0800

    first commit 2

--- src/test/java/red/zki/Springboot09SyncAsyncApplicationTests.java
+++ src/test/java/red/zki/Springboot09SyncAsyncApplicationTests.java
@@ -1,13 +1,32 @@
 package red.zki;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @SpringBootTest
 class Springboot09SyncAsyncApplicationTests {
 
+
+    @Autowired
+    JavaMailSenderImpl mailSender;
+
     @Test
     void contextLoads() {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setSubject("你好啊");
+        message.setText("OK OK OK");
+
+        message.setTo("727447170@qq.com");
+        message.setFrom("2849213279@qq.com");
+
+
+        mailSender.send(message);
     }
 
 }
